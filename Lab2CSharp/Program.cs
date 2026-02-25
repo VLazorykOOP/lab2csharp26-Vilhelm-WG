93ea604a559fc205491e5eefabc04ff136d3c4e7
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-02-25 20:31:12 +0200

    Make a menu for Lab_2

--- Lab2CSharp/Program.cs
+++ Lab2CSharp/Program.cs
@@ -4,7 +4,56 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Lab 2 CSharp");
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Lab 1 - меню");
+                Console.WriteLine("6 - Task 5 (Формула)");
+                Console.WriteLine("5 - Task 5 (Добуток цілих чисел)");
+                Console.WriteLine("4 - Task 4 (Масті)");
+                Console.WriteLine("3 - Task 3 (перевірка належності точки)");
+                Console.WriteLine("2 - Task 2 (перевірка парності)");
+                Console.WriteLine("1 - Task 1 (відстань між точками)");
+                Console.WriteLine("0 - Вихід");
+                Console.Write("Ваш вибір: ");
+
+                string? choice = Console.ReadLine();
+
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case "6":
+                        Task_6.Run();
+                        break;
+        
+                    case "5":
+                        Task_5.Run();
+                        break;
+        
+                    case "4":
+                        Task_4.Run();
+                        break;
+        
+                    case "3":
+                        Task_3.Run();
+                        break;
+        
+                    case "2":
+                        Task_2.Run();
+                        break;
+        
+                    case "1":
+                        Task1.Run();
+                        break;
+
+                    case "0":
+                        return;
+
+                    default:
+                        Console.WriteLine("Невірний вибір. Спробуйте ще раз.");
+                        break;
+                }
         }
     }
 }
