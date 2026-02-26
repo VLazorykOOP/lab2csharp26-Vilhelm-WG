68f11fcc9d2fa6596d3954ab39532807b03af51f
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-02-26 15:50:47 +0200

    update Task_3.cs

--- Lab2CSharp/Task_3.cs
+++ Lab2CSharp/Task_3.cs
@@ -2,32 +2,49 @@ namespace Lab2CSharp;
 
 public class Task_3
 {
-    public static void Run()
-    {
-        Console.WriteLine("Вихідний масив:");
-    }
-    static double[] Input()
-    {
-        Console.WriteLine("--- Task_2 ---");
-        Console.Write("Введіть розмірність масиву: ");
-            
-        if (!int.TryParse(Console.ReadLine(), out int n) || n <= 0)
+
+        static int[,] Input(out int n)
         {
-            Console.WriteLine("Помилка: розмірність має бути додатним числом.");
-            return new double[0];
-        }
-            
-        double[] a = new double[n];
-        for (int i = 0; i < n; ++i)
-        {
-            Console.Write($"a[{i}]= ");
-            // Додано TryParse для безпеки
-            if (!double.TryParse(Console.ReadLine(), out a[i]))
+            Console.WriteLine("--- Task_3 ---");
+            Console.Write("Введіть розмірність масиву: ");
+
+            if (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
             {
-                Console.WriteLine("Помилка введення, встановлено 0.");
-                a[i] = 0;
+                Console.WriteLine("Помилка: розмірність має бути додатним числом.");
+                return new int[0, 0];
+            }
+
+            int[,] a = new int[n, n];
+            for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+            {
+                Console.Write("a[{0},{1}]= ", i, j);
+                a[i, j] = int.Parse(Console.ReadLine());
+            }
+
+            return a;
+        }
+        static void Print(int[,] a, int n)
+        {
+            Console.WriteLine("Введена матриця: ");
+            if (a.Length == 0) Console.WriteLine("Масив порожній.");
+            else for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    // Форматуємо вивід, щоб числа стояли рівно (відступ у 4 символи)
+                    Console.Write($"{a[i, j],4} ");
+                }
+                Console.WriteLine();
             }
         }
-        return a;
+        
+
+        public static void Run()
+        {
+            int n;
+            int[,] Matrix = Input(out n);
+
+            Print(Matrix, n);
+        }
     }
-}
